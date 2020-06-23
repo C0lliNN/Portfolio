9f057019e102b6ad3be1ef05850b10ebefbbb33a
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2020-06-23 18:29:31 -0300

    Two new Projects Added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,12 +1,25 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { FormattedMessage, injectIntl } from 'react-intl';
+import easyVestImg from '../../assets/img/easy_vest.png';
 import hardwareSThorImg from '../../assets/img/hardware_sthor.png';
 import movieLibraryImg from '../../assets/img/movie_library.png';
+import shortlyImg from '../../assets/img/shortly.png';
 import Project from './Project/Project';
 
 const Projects = (props) => {
   const data = [
+    {
+      img: easyVestImg,
+      title: 'Easy Vest',
+      description: props.intl.formatMessage({
+        id: 'EasyVestDescription',
+        defaultMessage:
+          'A Application developed in Laravel + React that allow users create and share Questions and Lists of Questions.'
+      }),
+      demoLink: 'http://easy-vest.herokuapp.com/',
+      sourceLink: 'https://github.com/C0lliNN/EasyVest'
+    },
     {
       img: hardwareSThorImg,
       title: 'HardwareSThor',
@@ -27,6 +40,17 @@ const Projects = (props) => {
       }),
       demoLink: 'https://c0llinn.github.io/MovieLibrary/',
       sourceLink: 'https://github.com/C0lliNN/MovieLibrary'
+    },
+    {
+      img: shortlyImg,
+      title: 'Shortly',
+      description: props.intl.formatMessage({
+        id: 'ShortlyDescription',
+        defaultMessage:
+          'A Landing Page for URL Shortener developed in HTML, LESS and Vanilla JS'
+      }),
+      demoLink: 'https://c0llinn.github.io/Shortly/',
+      sourceLink: 'https://github.com/C0lliNN/Shortly'
     }
   ];
 
