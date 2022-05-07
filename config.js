396f80cf1c8ecc4206bc30121faa50d3a389adb8
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2022-05-07 14:05:13 -0300

    Update gatsby config

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ module.exports = {
   siteTitle: 'Raphael Collin',
   manifestName: 'Raphael Collin Portfolio',
   manifestShortName: 'Raphael Collin', // max 12 characters
-  manifestStartUrl: '/',
+  manifestStartUrl: '/Portfolio',
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/Portfolio`, // This path is subpath of your hosting https://domain/portfolio
   firstName: 'Raphael',
   lastName: 'Collin',
   email: 'raphael_professional@yahoo.com'
